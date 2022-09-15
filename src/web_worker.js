e3006830f51462b9b7c974dc1c3b59e9aa4d4488
dAxpeDDa <daxpedda@gmail.com>
2022-09-15 19:15:59 +0200

    Setup testing

--- src/web_worker.js
+++ src/web_worker.js
@@ -1,18 +1,16 @@
-let wasm;
-
 function __wasm_worker_try(f) {
-    try {
-        f();
-    } catch (e) {
-        return e;
-    }
+	try {
+		f();
+	} catch (e) {
+		return e;
+	}
 }
 
 self.onmessage = async event => {
-    let [module, memory, work] = event.data;
+	let [module, memory, work] = event.data;
 
-    wasm = await wasm_bindgen.initWithoutStart(module, memory);
-    await wasm.__wasm_worker_entry(work);
-    wasm.__wbindgen_thread_destroy();
-    self.close();
+	let wasm = await wasm_bindgen.initWithoutStart(module, memory);
+	await wasm.__wasm_worker_entry(work);
+	wasm.__wbindgen_thread_destroy();
+	self.close();
 };
