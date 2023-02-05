32cba720d23cacee8cf6e0cb5f837c0b95acf0f8
dAxpeDDa <daxpedda@gmail.com>
2023-02-05 20:36:43 +0100

    Move `WorkerOrContext` into module

---
+++ src/dedicated/common.rs
@@ -0,0 +1,64 @@
+use js_sys::Array;
+use wasm_bindgen::{JsValue, UnwrapThrowExt};
+use web_sys::{DedicatedWorkerGlobalScope, Worker};
+
+use crate::Message;
+
+pub(super) enum WorkerOrContext<'this> {
+	Worker(&'this Worker),
+	Context(&'this DedicatedWorkerGlobalScope),
+}
+
+impl WorkerOrContext<'_> {
+	fn post_message_with_transfer(
+		self,
+		message: &JsValue,
+		transfer: &JsValue,
+	) -> Result<(), JsValue> {
+		match self {
+			WorkerOrContext::Worker(worker) => worker.post_message_with_transfer(message, transfer),
+			WorkerOrContext::Context(context) => {
+				context.post_message_with_transfer(message, transfer)
+			}
+		}
+	}
+
+	pub(super) fn transfer_messages<M: IntoIterator<Item = Message>>(self, messages: M) {
+		let mut messages = messages.into_iter().map(Message::into_js_value);
+
+		let array = 'array: {
+			let Some(message_1) = messages.next() else {
+				return
+			};
+
+			let Some(message_2) = messages.next() else {
+				return self
+					.post_message_with_transfer(&message_1, &Array::of1(&message_1))
+					.unwrap_throw();
+			};
+
+			let Some(message_3) = messages.next() else {
+				break 'array  Array::of2(&message_1, &message_2);
+			};
+
+			let Some(message_4) = messages.next() else {
+				break 'array Array::of3(&message_1, &message_2, &message_3);
+			};
+
+			if let Some(message_5) = messages.next() {
+				let array = Array::of5(&message_1, &message_2, &message_3, &message_4, &message_5);
+
+				for message in messages {
+					array.push(&message);
+				}
+
+				array
+			} else {
+				Array::of4(&message_1, &message_2, &message_3, &message_4)
+			}
+		};
+
+		self.post_message_with_transfer(&array, &array)
+			.unwrap_throw();
+	}
+}
--- src/dedicated/mod.rs
+++ src/dedicated/mod.rs
@@ -1,4 +1,5 @@
 mod builder;
+mod common;
 mod context;
 mod handle;
 
@@ -6,13 +7,13 @@ use std::future::Future;
 use std::ops::Range;
 
 use js_sys::Array;
-use wasm_bindgen::{JsCast, JsValue, UnwrapThrowExt};
-use web_sys::{DedicatedWorkerGlobalScope, Worker};
+use wasm_bindgen::{JsCast, JsValue};
 
 pub use self::builder::{ModuleSupportError, WorkerBuilder};
+use self::common::WorkerOrContext;
 pub use self::context::WorkerContext;
 pub use self::handle::WorkerHandle;
-use crate::{Message, RawMessage};
+use crate::RawMessage;
 
 pub fn spawn<F1, F2>(f: F1) -> WorkerHandle
 where
@@ -20,65 +21,6 @@ where
 	F2: 'static + Future<Output = Close>,
 {
 	WorkerBuilder::new().unwrap().spawn(f)
-}
-
-enum WorkerOrContext<'this> {
-	Worker(&'this Worker),
-	Context(&'this DedicatedWorkerGlobalScope),
-}
-
-impl WorkerOrContext<'_> {
-	fn post_message_with_transfer(
-		self,
-		message: &JsValue,
-		transfer: &JsValue,
-	) -> Result<(), JsValue> {
-		match self {
-			WorkerOrContext::Worker(worker) => worker.post_message_with_transfer(message, transfer),
-			WorkerOrContext::Context(context) => {
-				context.post_message_with_transfer(message, transfer)
-			}
-		}
-	}
-
-	fn transfer_messages<M: IntoIterator<Item = Message>>(self, messages: M) {
-		let mut messages = messages.into_iter().map(Message::into_js_value);
-
-		let array = 'array: {
-			let Some(message_1) = messages.next() else {
-				return
-			};
-
-			let Some(message_2) = messages.next() else {
-				return self
-					.post_message_with_transfer(&message_1, &Array::of1(&message_1))
-					.unwrap_throw();
-			};
-
-			let Some(message_3) = messages.next() else {
-				break 'array  Array::of2(&message_1, &message_2);
-			};
-
-			let Some(message_4) = messages.next() else {
-				break 'array Array::of3(&message_1, &message_2, &message_3);
-			};
-
-			if let Some(message_5) = messages.next() {
-				let array = Array::of5(&message_1, &message_2, &message_3, &message_4, &message_5);
-
-				for message in messages {
-					array.push(&message);
-				}
-
-				array
-			} else {
-				Array::of4(&message_1, &message_2, &message_3, &message_4)
-			}
-		};
-
-		self.post_message_with_transfer(&array, &array)
-			.unwrap_throw();
-	}
 }
 
 #[derive(Debug)]
