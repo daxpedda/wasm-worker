a9d41792fbbfa631c7c31238505349593327ad49
dAxpeDDa <daxpedda@gmail.com>
2023-02-05 16:32:48 +0100

    Test `WorkerBuilder::has_module_support()`

--- tests/no_module_support.rs
+++ tests/no_module_support.rs
@@ -4,7 +4,12 @@ use wasm_worker::{ModuleSupportError, WorkerBuilder};
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn no_module_support() {
+fn check() {
+	assert!(!WorkerBuilder::has_module_support());
+}
+
+#[wasm_bindgen_test]
+fn builder_error() {
 	assert!(matches!(
 		WorkerBuilder::new().unwrap_err(),
 		ModuleSupportError,
