124a02c6c6cca1c773c047af44081db8b3eac58b
dAxpeDDa <daxpedda@gmail.com>
2023-02-05 16:22:14 +0100

    Remove duplicate transfer test from basic test

--- tests/basic.rs
+++ tests/basic.rs
@@ -5,10 +5,8 @@ mod util;
 use std::time::Duration;
 
 use futures_util::future::{self, Either};
-use js_sys::{ArrayBuffer, Uint8Array};
-use wasm_bindgen::JsValue;
 use wasm_bindgen_test::wasm_bindgen_test;
-use wasm_worker::{Close, Message, WorkerBuilder};
+use wasm_worker::Close;
 
 use self::util::Flag;
 
@@ -105,65 +103,4 @@ async fn terminate() {
 
 	let result = future::select(response_flag, util::sleep(Duration::from_millis(250))).await;
 	assert!(matches!(result, Either::Right(((), _))));
-}
-
-#[wasm_bindgen_test]
-async fn array_buffer_transfer() -> Result<(), JsValue> {
-	let buffer = ArrayBuffer::new(1);
-	let array = Uint8Array::new(&buffer);
-	array.copy_from(&[42]);
-
-	let flag_start = Flag::new();
-	let flag_sent = Flag::new();
-	let flag_finish = Flag::new();
-
-	let worker = WorkerBuilder::new()?
-		.set_message_handler({
-			let flag_finish = flag_finish.clone();
-			move |event| {
-				if let Some(Message::ArrayBuffer(buffer)) = event.message() {
-					let array = Uint8Array::new(&buffer);
-					assert_eq!(array.get_index(0), 42);
-				} else {
-					panic!()
-				}
-
-				flag_finish.signal();
-			}
-		})
-		.spawn({
-			let flag_start = flag_start.clone();
-			let flag_sent = flag_sent.clone();
-			|context| async move {
-				context.set_message_handler(move |context, event| {
-					if let Some(Message::ArrayBuffer(buffer)) = event.message() {
-						let array = Uint8Array::new(&buffer);
-						assert_eq!(array.get_index(0), 42);
-
-						assert!(context.transfer_message(buffer.into()).is_ok());
-					} else {
-						panic!()
-					}
-
-					flag_sent.signal();
-				});
-
-				flag_start.signal();
-
-				Close::No
-			}
-		});
-
-	flag_start.await;
-
-	assert!(worker
-		.transfer_message(Message::ArrayBuffer(buffer))
-		.is_ok());
-
-	flag_sent.await;
-	flag_finish.await;
-
-	worker.terminate();
-
-	Ok(())
 }
