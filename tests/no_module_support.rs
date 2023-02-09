6ab754ee1e38c78da212fc0c58e5deec483378e1
dAxpeDDa <daxpedda@gmail.com>
2023-02-09 21:37:05 +0100

    Clean up no ES module support tests

--- tests/no_module_support.rs
+++ tests/no_module_support.rs
@@ -1,41 +1,68 @@
+//! Tests for failures in various functions when ES modules are not supported
+//! but the ES module target was used when compiling the JS shim.
+//!
+//! Not tested by default!
+
 use wasm_bindgen::ShimFormat;
 use wasm_bindgen_test::wasm_bindgen_test;
 use wasm_worker::{ModuleSupportError, WorkerBuilder, WorkerUrl, WorkerUrlFormat};
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
+/// [`wasm_worker::spawn()`].
 #[wasm_bindgen_test]
-fn assert_target() {
+#[should_panic(expected = "ModuleSupportError")]
+fn spawn() {
 	assert!(matches!(
 		wasm_bindgen::shim_format(),
 		Some(ShimFormat::EsModule)
 	));
-}
 
-#[wasm_bindgen_test]
-#[should_panic(expected = "ModuleSupportError")]
-fn spawn() {
 	wasm_worker::spawn(|_| unreachable!());
 }
 
+/// [`wasm_worker::spawn_async()`].
 #[wasm_bindgen_test]
 #[should_panic(expected = "ModuleSupportError")]
 fn spawn_async() {
+	assert!(matches!(
+		wasm_bindgen::shim_format(),
+		Some(ShimFormat::EsModule)
+	));
+
 	wasm_worker::spawn_async(|_| async { unreachable!() });
 }
 
+/// [`WorkerBuilder::has_module_support()`].
 #[wasm_bindgen_test]
 fn check() {
+	assert!(matches!(
+		wasm_bindgen::shim_format(),
+		Some(ShimFormat::EsModule)
+	));
+
 	assert!(!WorkerBuilder::has_module_support());
 }
 
+/// [`WorkerBuilder::new()`].
 #[wasm_bindgen_test]
 fn builder() {
+	assert!(matches!(
+		wasm_bindgen::shim_format(),
+		Some(ShimFormat::EsModule)
+	));
+
 	assert_eq!(WorkerBuilder::new().unwrap_err(), ModuleSupportError);
 }
 
+/// [`WorkerBuilder::new_with_url()`].
 #[wasm_bindgen_test]
 fn builder_url() {
+	assert!(matches!(
+		wasm_bindgen::shim_format(),
+		Some(ShimFormat::EsModule)
+	));
+
 	let url = WorkerUrl::new(
 		&wasm_bindgen::shim_url().unwrap(),
 		WorkerUrlFormat::EsModule,
