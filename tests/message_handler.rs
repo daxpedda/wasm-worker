13acd8dc29fe130894edd4c5d93cf71d8ca17685
dAxpeDDa <daxpedda@gmail.com>
2023-02-09 14:38:07 +0100

    Rename tests

---
+++ tests/message_handler.rs
@@ -0,0 +1,189 @@
+#![allow(unreachable_pub)]
+
+mod util;
+
+use std::time::Duration;
+
+use futures_util::future::{self, Either};
+use futures_util::FutureExt;
+use js_sys::{ArrayBuffer, Uint8Array};
+use wasm_bindgen::JsValue;
+use wasm_bindgen_test::wasm_bindgen_test;
+use wasm_worker::{Close, Message, WorkerBuilder};
+
+use self::util::Flag;
+
+wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+async fn clear_message_handler() -> Result<(), JsValue> {
+	assert!(Message::has_array_buffer_support());
+
+	let buffer = ArrayBuffer::new(1);
+
+	let flag_spawner_start = Flag::new();
+	let flag_worker_start = Flag::new();
+	let flag_spawner_received = Flag::new();
+	let flag_worker_received = Flag::new();
+
+	let worker = WorkerBuilder::new()?
+		.message_handler({
+			let flag_received = flag_spawner_received.clone();
+			move |_, _| flag_received.signal()
+		})
+		.spawn_async({
+			let flag_spawner_start = flag_spawner_start.clone();
+			let flag_worker_start = flag_worker_start.clone();
+			let flag_received = flag_worker_received.clone();
+			|context| async move {
+				context.set_message_handler(move |_, _| flag_received.signal());
+				context.clear_message_handler();
+
+				flag_worker_start.signal();
+				flag_spawner_start.await;
+
+				let buffer = ArrayBuffer::new(1);
+				context.transfer_messages([buffer]);
+
+				Close::No
+			}
+		});
+
+	worker.clear_message_handler();
+
+	flag_spawner_start.signal();
+	flag_worker_start.await;
+
+	worker.transfer_messages([buffer]);
+
+	let result = future::select_all([
+		flag_spawner_received.map(|_| true).boxed_local(),
+		flag_worker_received.map(|_| true).boxed_local(),
+		util::sleep(Duration::from_millis(250))
+			.map(|_| false)
+			.boxed_local(),
+	])
+	.await;
+	assert!(!result.0);
+
+	worker.terminate();
+
+	Ok(())
+}
+
+#[wasm_bindgen_test]
+fn has_message_handler() -> Result<(), JsValue> {
+	let worker = WorkerBuilder::new()?
+		.message_handler(|_, _| ())
+		.spawn(|_| Close::Yes);
+	assert!(worker.has_message_handler());
+	worker.clear_message_handler();
+	assert!(!worker.has_message_handler());
+
+	let worker = wasm_worker::spawn(|_| Close::Yes);
+
+	assert!(!worker.has_message_handler());
+	worker.set_message_handler(|_, _| ());
+	assert!(worker.has_message_handler());
+	worker.clear_message_handler();
+	assert!(!worker.has_message_handler());
+
+	Ok(())
+}
+
+#[wasm_bindgen_test]
+async fn multi_messages() -> Result<(), JsValue> {
+	assert!(Message::has_array_buffer_support());
+
+	let flag = Flag::new();
+
+	let worker = WorkerBuilder::new()?
+		.message_handler(move |context, event| {
+			let messages: Vec<ArrayBuffer> = event
+				.messages()
+				.map(|message| message.serialize_as().unwrap())
+				.collect();
+
+			assert_eq!(messages.len(), 3);
+
+			for (index, buffer) in (1_u8..).zip(&messages) {
+				let array = Uint8Array::new(buffer);
+				assert_eq!(buffer.byte_length(), index.into());
+				let mut output = [0; 3];
+				array.copy_to(&mut output[..index.into()]);
+				assert!(output[..index.into()].iter().all(|value| *value == index));
+			}
+
+			context.transfer_messages(messages);
+		})
+		.spawn_async({
+			let flag = flag.clone();
+			|context| async move {
+				context.set_message_handler(move |_, event| {
+					let messages = event.messages();
+					assert_eq!(messages.len(), 3);
+
+					for (index, message) in (1_u8..).zip(messages) {
+						let buffer: ArrayBuffer = message.serialize_as().unwrap();
+						let array = Uint8Array::new(&buffer);
+						assert_eq!(buffer.byte_length(), index.into());
+						let mut output = [0; 3];
+						array.copy_to(&mut output[..index.into()]);
+						assert!(output[..index.into()].iter().all(|value| *value == index));
+					}
+
+					flag.signal();
+				});
+
+				let buffer_1 = ArrayBuffer::new(1);
+				let array = Uint8Array::new(&buffer_1);
+				array.copy_from(&[1]);
+
+				let buffer_2 = ArrayBuffer::new(2);
+				let array = Uint8Array::new(&buffer_2);
+				array.copy_from(&[2; 2]);
+
+				let buffer_3 = ArrayBuffer::new(3);
+				let array = Uint8Array::new(&buffer_3);
+				array.copy_from(&[3; 3]);
+
+				context.transfer_messages([buffer_1, buffer_2, buffer_3]);
+
+				Close::No
+			}
+		});
+
+	flag.await;
+
+	worker.terminate();
+
+	Ok(())
+}
+
+#[wasm_bindgen_test]
+async fn async_message_handler() {
+	assert!(Message::has_array_buffer_support());
+
+	let flag = Flag::new();
+
+	let worker = wasm_worker::spawn(|context| {
+		let buffer = ArrayBuffer::new(1);
+		context.transfer_messages([buffer]);
+
+		Close::Yes
+	});
+
+	worker.set_message_handler_async({
+		let flag = flag.clone();
+		move |_, _| {
+			let flag = flag.clone();
+			async move {
+				util::sleep(Duration::from_millis(250)).await;
+				flag.signal();
+			}
+		}
+	});
+
+	let result = future::select(flag, util::sleep(Duration::from_millis(500))).await;
+	assert!(matches!(result, Either::Left(((), _))));
+}
--- tests/transfer.rs
+++ tests/transfer.rs
@@ -2,158 +2,70 @@
 
 mod util;
 
-use std::time::Duration;
-
-use futures_util::future::{self, Either};
-use futures_util::FutureExt;
 use js_sys::{ArrayBuffer, Uint8Array};
-use wasm_bindgen::JsValue;
+use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_test::wasm_bindgen_test;
 use wasm_worker::{Close, Message, WorkerBuilder};
+#[cfg(web_sys_unstable_apis)]
+use {
+	wasm_bindgen::UnwrapThrowExt,
+	web_sys::{AudioData, AudioDataCopyToOptions, AudioDataInit, AudioSampleFormat},
+};
 
 use self::util::Flag;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
-async fn clear_message_handler() -> Result<(), JsValue> {
-	assert!(Message::has_array_buffer_support());
+async fn test_transfer<T: JsCast + Into<Message>>(
+	support: impl FnOnce() -> bool,
+	force: bool,
+	init: impl FnOnce() -> T,
+	assert: impl 'static + Copy + Fn(&T) + Send,
+) -> Result<(), JsValue> {
+	if !support() {
+		if force {
+			panic!("type unsupported in this browser")
+		} else {
+			return Ok(());
+		}
+	}
 
-	let buffer = ArrayBuffer::new(1);
+	let value = init();
 
-	let flag_spawner_start = Flag::new();
-	let flag_worker_start = Flag::new();
-	let flag_spawner_received = Flag::new();
-	let flag_worker_received = Flag::new();
+	let flag_start = Flag::new();
+	let flag_finish = Flag::new();
 
 	let worker = WorkerBuilder::new()?
 		.message_handler({
-			let flag_received = flag_spawner_received.clone();
-			move |_, _| flag_received.signal()
-		})
-		.spawn_async({
-			let flag_spawner_start = flag_spawner_start.clone();
-			let flag_worker_start = flag_worker_start.clone();
-			let flag_received = flag_worker_received.clone();
-			|context| async move {
-				context.set_message_handler(move |_, _| flag_received.signal());
-				context.clear_message_handler();
+			let flag_finish = flag_finish.clone();
+			move |_, event| {
+				let value: T = event.messages().next().unwrap().serialize_as().unwrap();
 
-				flag_worker_start.signal();
-				flag_spawner_start.await;
+				assert(&value);
 
-				let buffer = ArrayBuffer::new(1);
-				context.transfer_messages([buffer]);
-
-				Close::No
+				flag_finish.signal();
 			}
-		});
-
-	worker.clear_message_handler();
-
-	flag_spawner_start.signal();
-	flag_worker_start.await;
-
-	worker.transfer_messages([buffer]);
-
-	let result = future::select_all([
-		flag_spawner_received.map(|_| true).boxed_local(),
-		flag_worker_received.map(|_| true).boxed_local(),
-		util::sleep(Duration::from_millis(250))
-			.map(|_| false)
-			.boxed_local(),
-	])
-	.await;
-	assert!(!result.0);
-
-	worker.terminate();
-
-	Ok(())
-}
-
-#[wasm_bindgen_test]
-fn has_message_handler() -> Result<(), JsValue> {
-	let worker = WorkerBuilder::new()?
-		.message_handler(|_, _| ())
-		.spawn(|_| Close::Yes);
-	assert!(worker.has_message_handler());
-	worker.clear_message_handler();
-	assert!(!worker.has_message_handler());
-
-	let worker = wasm_worker::spawn(|_| Close::Yes);
-
-	assert!(!worker.has_message_handler());
-	worker.set_message_handler(|_, _| ());
-	assert!(worker.has_message_handler());
-	worker.clear_message_handler();
-	assert!(!worker.has_message_handler());
-
-	Ok(())
-}
-
-#[wasm_bindgen_test]
-async fn multi_messages() -> Result<(), JsValue> {
-	assert!(Message::has_array_buffer_support());
-
-	let flag = Flag::new();
-
-	let worker = WorkerBuilder::new()?
-		.message_handler(move |context, event| {
-			let messages: Vec<ArrayBuffer> = event
-				.messages()
-				.map(|message| message.serialize_as().unwrap())
-				.collect();
-
-			assert_eq!(messages.len(), 3);
-
-			for (index, buffer) in (1_u8..).zip(&messages) {
-				let array = Uint8Array::new(buffer);
-				assert_eq!(buffer.byte_length(), index.into());
-				let mut output = [0; 3];
-				array.copy_to(&mut output[..index.into()]);
-				assert!(output[..index.into()].iter().all(|value| *value == index));
-			}
-
-			context.transfer_messages(messages);
 		})
-		.spawn_async({
-			let flag = flag.clone();
-			|context| async move {
-				context.set_message_handler(move |_, event| {
-					let messages = event.messages();
-					assert_eq!(messages.len(), 3);
+		.spawn({
+			let flag_start = flag_start.clone();
+			move |context| {
+				context.set_message_handler(move |context, event| {
+					let value: T = event.messages().next().unwrap().serialize_as().unwrap();
 
-					for (index, message) in (1_u8..).zip(messages) {
-						let buffer: ArrayBuffer = message.serialize_as().unwrap();
-						let array = Uint8Array::new(&buffer);
-						assert_eq!(buffer.byte_length(), index.into());
-						let mut output = [0; 3];
-						array.copy_to(&mut output[..index.into()]);
-						assert!(output[..index.into()].iter().all(|value| *value == index));
-					}
+					assert(&value);
 
-					flag.signal();
+					context.transfer_messages([value]);
 				});
 
-				let buffer_1 = ArrayBuffer::new(1);
-				let array = Uint8Array::new(&buffer_1);
-				array.copy_from(&[1]);
-
-				let buffer_2 = ArrayBuffer::new(2);
-				let array = Uint8Array::new(&buffer_2);
-				array.copy_from(&[2; 2]);
-
-				let buffer_3 = ArrayBuffer::new(3);
-				let array = Uint8Array::new(&buffer_3);
-				array.copy_from(&[3; 3]);
-
-				context.transfer_messages([buffer_1, buffer_2, buffer_3]);
+				flag_start.signal();
 
 				Close::No
 			}
 		});
 
-	flag.await;
+	flag_start.await;
+	worker.transfer_messages([value]);
+	flag_finish.await;
 
 	worker.terminate();
 
@@ -161,29 +73,45 @@ async fn multi_messages() -> Result<(), JsValue> {
 }
 
 #[wasm_bindgen_test]
-async fn async_message_handler() {
-	assert!(Message::has_array_buffer_support());
+async fn array_buffer() -> Result<(), JsValue> {
+	test_transfer(
+		Message::has_array_buffer_support,
+		true,
+		|| {
+			let buffer = ArrayBuffer::new(1);
+			let array = Uint8Array::new(&buffer);
+			array.copy_from(&[42]);
+			buffer
+		},
+		|buffer| {
+			let array = Uint8Array::new(buffer);
+			assert_eq!(array.get_index(0), 42);
+		},
+	)
+	.await
+}
 
-	let flag = Flag::new();
-
-	let worker = wasm_worker::spawn(|context| {
-		let buffer = ArrayBuffer::new(1);
-		context.transfer_messages([buffer]);
-
-		Close::Yes
-	});
-
-	worker.set_message_handler_async({
-		let flag = flag.clone();
-		move |_, _| {
-			let flag = flag.clone();
-			async move {
-				util::sleep(Duration::from_millis(250)).await;
-				flag.signal();
-			}
-		}
-	});
-
-	let result = future::select(flag, util::sleep(Duration::from_millis(500))).await;
-	assert!(matches!(result, Either::Left(((), _))));
+#[wasm_bindgen_test]
+#[cfg(web_sys_unstable_apis)]
+async fn audio_data() -> Result<(), JsValue> {
+	test_transfer(
+		Message::has_audio_data_support,
+		false,
+		|| {
+			let init = AudioDataInit::new(
+				&ArrayBuffer::new(42),
+				AudioSampleFormat::U8,
+				1,
+				42,
+				3000.,
+				0.,
+			);
+			AudioData::new(&init).unwrap_throw()
+		},
+		|data| {
+			let size = data.allocation_size(&AudioDataCopyToOptions::new(0));
+			assert_eq!(size, 42);
+		},
+	)
+	.await
 }
