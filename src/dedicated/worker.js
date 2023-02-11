d1abadbb090585509d1db87347f3a2b88ecceb3d
dAxpeDDa <daxpedda@gmail.com>
2023-02-12 00:16:38 +0100

    Reduce size of `worker.js`

--- src/dedicated/worker.js
+++ src/dedicated/worker.js
@@ -1,10 +1,8 @@
-let __wasm_worker_wasm;
-
 self.onmessage = async __wasm_worker_event => {
 	self.onmessage = undefined;
 
 	const [__wasm_worker_module, __wasm_worker_memory, __wasm_worker_task] = __wasm_worker_event.data;
-	__wasm_worker_wasm = await __wasm_worker_wasm_bindgen(__wasm_worker_module, __wasm_worker_memory);
+	const __wasm_worker_wasm = await __wasm_worker_wasm_bindgen(__wasm_worker_module, __wasm_worker_memory);
 
 	if (await Promise.resolve(__wasm_worker_entry(__wasm_worker_task)) === true) {
 		__wasm_worker_wasm.__wbindgen_thread_destroy();
