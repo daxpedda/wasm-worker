4329d8748aab34eaa5e8dcb4094fc126327910d3
dAxpeDDa <daxpedda@gmail.com>
2023-02-13 22:37:49 +0100

    Fix example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,13 +1,13 @@
 #![allow(clippy::missing_docs_in_private_items, missing_docs)]
 
-use wasm_worker::Close;
 use web_sys::console;
 
 fn main() {
+	web_sys::console::log_1(&wasm_bindgen::exports());
+
 	console::log_1(&"start".into());
 
 	wasm_worker::spawn(|_| {
 		console::log_1(&"thread".into());
-		Close::Yes
 	});
 }
