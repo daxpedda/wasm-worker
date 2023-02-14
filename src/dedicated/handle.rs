84e747789b97e932df0dabc974f0a74d4113caf7
dAxpeDDa <daxpedda@gmail.com>
2023-02-14 13:32:22 +0100

    Refactor `WorkerHandle` and `WorkerHandleRef`

--- src/dedicated/handle.rs
+++ src/dedicated/handle.rs
@@ -1,3 +1,4 @@
+use std::borrow::Cow;
 use std::cell::{Cell, RefCell};
 use std::error::Error;
 use std::fmt::{self, Debug, Display, Formatter};
@@ -32,6 +33,28 @@ impl PartialEq for WorkerHandle {
 	}
 }
 
+impl WorkerHandleOrRef for WorkerHandle {
+	fn handle_ref(&self) -> WorkerHandleRef {
+		WorkerHandleRef {
+			worker: self.worker.clone(),
+			id: Rc::clone(&self.id),
+			message_handler: Rc::downgrade(&self.message_handler),
+		}
+	}
+
+	fn worker(&self) -> &Worker {
+		&self.worker
+	}
+
+	fn id(&self) -> &Rc<Cell<Option<usize>>> {
+		&self.id
+	}
+
+	fn message_handler(&self) -> Option<Cow<'_, Rc<RefCell<Option<Closure>>>>> {
+		Some(Cow::Borrowed(&self.message_handler))
+	}
+}
+
 impl WorkerHandle {
 	pub(super) fn new(
 		worker: Worker,
@@ -51,87 +74,51 @@ impl WorkerHandle {
 	}
 
 	#[must_use]
+	#[allow(clippy::same_name_method)]
 	pub fn has_message_handler(&self) -> bool {
-		RefCell::borrow(&self.message_handler).is_some()
+		<Self as WorkerHandleOrRef>::has_message_handler(self)
 	}
 
-	#[allow(clippy::must_use_candidate)]
+	#[allow(clippy::same_name_method)]
 	pub fn clear_message_handler(&self) {
-		RefCell::borrow_mut(&self.message_handler).take();
-		self.worker.set_onmessage(None);
+		<Self as WorkerHandleOrRef>::clear_message_handler(self);
 	}
 
+	#[allow(clippy::same_name_method)]
 	pub fn set_message_handler<F: 'static + FnMut(&WorkerHandleRef, MessageEvent)>(
 		&self,
-		mut new_message_handler: F,
+		new_message_handler: F,
 	) {
-		let handle = WorkerHandleRef {
-			worker: self.worker.clone(),
-			id: Rc::clone(&self.id),
-			message_handler: Rc::downgrade(&self.message_handler),
-		};
-
-		let mut message_handler = RefCell::borrow_mut(&self.message_handler);
-		let message_handler = message_handler.insert(Closure::classic(move |event| {
-			new_message_handler(&handle, MessageEvent::new(event));
-		}));
-
-		self.worker.set_onmessage(Some(message_handler));
+		<Self as WorkerHandleOrRef>::set_message_handler(self, new_message_handler);
 	}
 
+	#[allow(clippy::same_name_method)]
 	pub fn set_message_handler_async<
 		F1: 'static + FnMut(&WorkerHandleRef, MessageEvent) -> F2,
 		F2: 'static + Future<Output = ()>,
 	>(
 		&self,
-		mut new_message_handler: F1,
+		new_message_handler: F1,
 	) {
-		let handle = WorkerHandleRef {
-			worker: self.worker.clone(),
-			id: Rc::clone(&self.id),
-			message_handler: Rc::downgrade(&self.message_handler),
-		};
-
-		let mut message_handler = RefCell::borrow_mut(&self.message_handler);
-		let message_handler = message_handler.insert(Closure::future(move |event| {
-			new_message_handler(&handle, MessageEvent::new(event))
-		}));
-
-		self.worker.set_onmessage(Some(message_handler));
+		<Self as WorkerHandleOrRef>::set_message_handler_async(self, new_message_handler);
 	}
 
+	#[allow(clippy::same_name_method)]
 	pub fn transfer_messages<M: IntoIterator<Item = I>, I: Into<Message>>(
 		&self,
 		messages: M,
 	) -> Result<(), TransferError> {
-		WorkerOrContext::Worker(&self.worker).transfer_messages(messages)
+		<Self as WorkerHandleOrRef>::transfer_messages(self, messages)
 	}
 
+	#[allow(clippy::same_name_method)]
 	pub fn terminate(&self) {
-		self.worker.terminate();
+		<Self as WorkerHandleOrRef>::terminate(self);
 	}
 
+	#[allow(clippy::same_name_method)]
 	pub fn destroy(self, tls: Tls) -> Result<(), DestroyError<Self>> {
-		if let Some(id) = self.id.get() {
-			if id == tls.id {
-				self.id.take();
-				self.terminate();
-
-				EXPORTS.with(|exports| {
-					// SAFETY: The id is uniquely created in `WorkerBuilder::spawn_internal()`
-					// through an `AtomicUsize` counter. It then is saved here and sent to the
-					// worker and used in generating `Tls`. The ids are then compared above and if
-					// they match, the state is change to `None` preventing any subsequent calls.
-					unsafe { exports.thread_destroy(&tls.tls_base(), &tls.stack_alloc()) };
-				});
-
-				Ok(())
-			} else {
-				Err(DestroyError::Match { handle: self, tls })
-			}
-		} else {
-			Err(DestroyError::Already(tls))
-		}
+		<Self as WorkerHandleOrRef>::destroy(self, tls)
 	}
 }
 
@@ -140,6 +127,24 @@ pub struct WorkerHandleRef {
 	worker: Worker,
 	id: Rc<Cell<Option<usize>>>,
 	message_handler: Weak<RefCell<Option<Closure>>>,
+}
+
+impl WorkerHandleOrRef for WorkerHandleRef {
+	fn handle_ref(&self) -> WorkerHandleRef {
+		self.clone()
+	}
+
+	fn worker(&self) -> &Worker {
+		&self.worker
+	}
+
+	fn id(&self) -> &Rc<Cell<Option<usize>>> {
+		&self.id
+	}
+
+	fn message_handler(&self) -> Option<Cow<'_, Rc<RefCell<Option<Closure>>>>> {
+		Weak::upgrade(&self.message_handler).map(Cow::Owned)
+	}
 }
 
 impl WorkerHandleRef {
@@ -161,70 +166,126 @@ impl WorkerHandleRef {
 	}
 
 	#[must_use]
+	#[allow(clippy::same_name_method)]
 	pub fn has_message_handler(&self) -> bool {
-		Weak::upgrade(&self.message_handler).map_or(false, |message_handler| {
+		<Self as WorkerHandleOrRef>::has_message_handler(self)
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn clear_message_handler(&self) {
+		<Self as WorkerHandleOrRef>::clear_message_handler(self);
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn set_message_handler<F: 'static + FnMut(&Self, MessageEvent)>(
+		&self,
+		new_message_handler: F,
+	) {
+		<Self as WorkerHandleOrRef>::set_message_handler(self, new_message_handler);
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn set_message_handler_async<
+		F1: 'static + FnMut(&Self, MessageEvent) -> F2,
+		F2: 'static + Future<Output = ()>,
+	>(
+		&self,
+		new_message_handler: F1,
+	) {
+		<Self as WorkerHandleOrRef>::set_message_handler_async(self, new_message_handler);
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn transfer_messages<M: IntoIterator<Item = I>, I: Into<Message>>(
+		&self,
+		messages: M,
+	) -> Result<(), TransferError> {
+		<Self as WorkerHandleOrRef>::transfer_messages(self, messages)
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn terminate(&self) {
+		<Self as WorkerHandleOrRef>::terminate(self);
+	}
+
+	#[allow(clippy::same_name_method)]
+	pub fn destroy(self, tls: Tls) -> Result<(), DestroyError<Self>> {
+		<Self as WorkerHandleOrRef>::destroy(self, tls)
+	}
+}
+
+trait WorkerHandleOrRef: Debug + Sized {
+	fn handle_ref(&self) -> WorkerHandleRef;
+
+	fn worker(&self) -> &Worker;
+
+	fn id(&self) -> &Rc<Cell<Option<usize>>>;
+
+	fn message_handler(&self) -> Option<Cow<'_, Rc<RefCell<Option<Closure>>>>>;
+
+	fn has_message_handler(&self) -> bool {
+		self.message_handler().map_or(false, |message_handler| {
 			RefCell::borrow(&message_handler).is_some()
 		})
 	}
 
-	#[allow(clippy::must_use_candidate)]
-	pub fn clear_message_handler(&self) {
-		if let Some(messange_handler) = Weak::upgrade(&self.message_handler) {
-			messange_handler.take();
-			self.worker.set_onmessage(None);
+	fn clear_message_handler(&self) {
+		if let Some(message_handler) = self.message_handler() {
+			message_handler.take();
+			self.worker().set_onmessage(None);
 		}
 	}
 
-	pub fn set_message_handler<F: 'static + FnMut(&Self, MessageEvent)>(
+	fn set_message_handler<F: 'static + FnMut(&WorkerHandleRef, MessageEvent)>(
 		&self,
 		mut new_message_handler: F,
 	) {
-		if let Some(message_handler) = Weak::upgrade(&self.message_handler) {
-			let handle = self.clone();
+		if let Some(message_handler) = self.message_handler() {
+			let handle = self.handle_ref();
 
 			let mut message_handler = RefCell::borrow_mut(&message_handler);
 			let message_handler = message_handler.insert(Closure::classic(move |event| {
 				new_message_handler(&handle, MessageEvent::new(event));
 			}));
 
-			self.worker.set_onmessage(Some(message_handler));
+			self.worker().set_onmessage(Some(message_handler));
 		}
 	}
 
-	pub fn set_message_handler_async<
-		F1: 'static + FnMut(&Self, MessageEvent) -> F2,
+	fn set_message_handler_async<
+		F1: 'static + FnMut(&WorkerHandleRef, MessageEvent) -> F2,
 		F2: 'static + Future<Output = ()>,
 	>(
 		&self,
 		mut new_message_handler: F1,
 	) {
-		if let Some(message_handler) = Weak::upgrade(&self.message_handler) {
-			let handle = self.clone();
+		if let Some(message_handler) = self.message_handler() {
+			let handle = self.handle_ref();
 
 			let mut message_handler = RefCell::borrow_mut(&message_handler);
 			let message_handler = message_handler.insert(Closure::future(move |event| {
 				new_message_handler(&handle, MessageEvent::new(event))
 			}));
 
-			self.worker.set_onmessage(Some(message_handler));
+			self.worker().set_onmessage(Some(message_handler));
 		}
 	}
 
-	pub fn transfer_messages<M: IntoIterator<Item = I>, I: Into<Message>>(
+	fn transfer_messages<M: IntoIterator<Item = I>, I: Into<Message>>(
 		&self,
 		messages: M,
 	) -> Result<(), TransferError> {
-		WorkerOrContext::Worker(&self.worker).transfer_messages(messages)
+		WorkerOrContext::Worker(self.worker()).transfer_messages(messages)
 	}
 
-	pub fn terminate(&self) {
-		self.worker.terminate();
+	fn terminate(&self) {
+		self.worker().terminate();
 	}
 
-	pub fn destroy(self, tls: Tls) -> Result<(), DestroyError<Self>> {
-		if let Some(id) = self.id.get() {
+	fn destroy(self, tls: Tls) -> Result<(), DestroyError<Self>> {
+		if let Some(id) = self.id().get() {
 			if id == tls.id {
-				self.id.take();
+				self.id().take();
 				self.terminate();
 
 				EXPORTS.with(|exports| {
