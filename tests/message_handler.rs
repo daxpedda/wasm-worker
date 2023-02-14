2e9ef2c16ff018d9261b9ac4fd4c5640aac18d8e
dAxpeDDa <daxpedda@gmail.com>
2023-02-14 12:28:49 +0100

    Add message handler tests for `WorkerHandleRef`

--- tests/message_handler.rs
+++ tests/message_handler.rs
@@ -33,6 +33,8 @@ async fn builder_clear_message_handler() -> Result<()> {
 			|context| async move {
 				request.await;
 				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+				context.close();
 			}
 		});
 
@@ -42,8 +44,6 @@ async fn builder_clear_message_handler() -> Result<()> {
 	// The message handler will never respond if cleared.
 	let result = future::select(response, util::sleep(SIGNAL_DURATION)).await;
 	assert!(matches!(result, Either::Right(((), _))));
-
-	worker.terminate();
 
 	Ok(())
 }
@@ -62,6 +62,8 @@ async fn handle_clear_message_handler() {
 		|context| async move {
 			request.await;
 			context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+			context.close();
 		}
 	});
 
@@ -75,8 +77,46 @@ async fn handle_clear_message_handler() {
 	// The message handler will never respond if cleared.
 	let result = future::select(response, util::sleep(SIGNAL_DURATION)).await;
 	assert!(matches!(result, Either::Right(((), _))));
+}
 
-	worker.terminate();
+/// [`WorkerHandleRef::set_message_handler()`](wasm_worker::WorkerHandleRef::set_message_handler) with
+/// [`WorkerHandleRef::clear_message_handler()`](wasm_worker::WorkerHandleRef::clear_message_handler).
+#[wasm_bindgen_test]
+async fn handle_ref_clear_message_handler() {
+	assert!(Message::has_array_buffer_support().is_ok());
+
+	let request = Flag::new();
+	let response = Flag::new();
+
+	let worker = wasm_worker::spawn_async({
+		let request = request.clone();
+		|context| async move {
+			request.await;
+			context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+			context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+			context.close();
+		}
+	});
+
+	worker.set_message_handler({
+		let response = response.clone();
+		let mut cleared = false;
+		move |worker, _| {
+			worker.clear_message_handler();
+
+			if cleared {
+				response.signal();
+			} else {
+				cleared = true;
+			}
+		}
+	});
+	request.signal();
+
+	// The message handler will never respond if cleared.
+	let result = future::select(response, util::sleep(SIGNAL_DURATION)).await;
+	assert!(matches!(result, Either::Right(((), _))));
 }
 
 /// [`WorkerContext::set_message_handler()`] with
@@ -133,6 +173,36 @@ fn handle_has_message_handler() {
 	assert!(worker.has_message_handler());
 	worker.clear_message_handler();
 	assert!(!worker.has_message_handler());
+}
+
+/// [`WorkerHandleRef::set_message_handler()`](wasm_worker::WorkerHandleRef::set_message_handler) with
+/// [`WorkerHandleRef::has_message_handler()`](wasm_worker::WorkerHandleRef::has_message_handler).
+#[wasm_bindgen_test]
+async fn handle_ref_has_message_handler() -> Result<()> {
+	assert!(Message::has_array_buffer_support().is_ok());
+
+	let flag = Flag::new();
+
+	let _worker = WorkerBuilder::new()?
+		.message_handler({
+			let flag = flag.clone();
+			move |worker, _| {
+				assert!(worker.has_message_handler());
+				worker.clear_message_handler();
+				assert!(!worker.has_message_handler());
+
+				// Flag will never signal if `assert!`s panic.
+				flag.signal();
+			}
+		})
+		.spawn(|context| {
+			context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+			context.close();
+		});
+
+	flag.await;
+
+	Ok(())
 }
 
 /// [`WorkerContext::has_message_handler()`].
@@ -211,6 +281,37 @@ async fn handle_message_handler() {
 	response.await;
 }
 
+/// [`WorkerHandleRef::set_message_handler()`](wasm_worker::WorkerHandleRef::set_message_handler).
+#[wasm_bindgen_test]
+async fn handle_ref_message_handler() -> Result<()> {
+	assert!(Message::has_array_buffer_support().is_ok());
+
+	let flag = Flag::new();
+
+	let _worker = WorkerBuilder::new()?
+		.message_handler({
+			let flag = flag.clone();
+			move |worker, _| {
+				worker.set_message_handler({
+					let flag = flag.clone();
+					move |_, _| flag.signal()
+				});
+			}
+		})
+		.spawn({
+			|context| {
+				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+				context.close();
+			}
+		});
+
+	flag.await;
+
+	Ok(())
+}
+
 /// [`WorkerContext::set_message_handler()`].
 #[wasm_bindgen_test]
 async fn context_message_handler() {
@@ -238,7 +339,7 @@ async fn context_message_handler() {
 
 /// [`WorkerBuilder::message_handler_async()`].
 #[wasm_bindgen_test]
-async fn builder_async_message_handler() -> Result<()> {
+async fn builder_message_handler_async() -> Result<()> {
 	assert!(Message::has_array_buffer_support().is_ok());
 
 	let flag = Flag::new();
@@ -266,7 +367,7 @@ async fn builder_async_message_handler() -> Result<()> {
 
 /// [`WorkerHandle::set_message_handler_async()`](wasm_worker::WorkerHandle::set_message_handler_async).
 #[wasm_bindgen_test]
-async fn handle_async_message_handler() {
+async fn handle_message_handler_async() {
 	assert!(Message::has_array_buffer_support().is_ok());
 
 	let request = Flag::new();
@@ -294,9 +395,43 @@ async fn handle_async_message_handler() {
 	response.await;
 }
 
+/// [`WorkerHandleRef::set_message_handler()`](wasm_worker::WorkerHandleRef::set_message_handler).
+#[wasm_bindgen_test]
+async fn handle_ref_message_handler_async() -> Result<()> {
+	assert!(Message::has_array_buffer_support().is_ok());
+
+	let flag = Flag::new();
+
+	let _worker = WorkerBuilder::new()?
+		.message_handler({
+			let flag = flag.clone();
+			move |worker, _| {
+				worker.set_message_handler_async({
+					let flag = flag.clone();
+					move |_, _| {
+						let flag = flag.clone();
+						async move { flag.signal() }
+					}
+				});
+			}
+		})
+		.spawn({
+			|context| {
+				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+				context.close();
+			}
+		});
+
+	flag.await;
+
+	Ok(())
+}
+
 /// [`WorkerContext::set_message_handler_async()`].
 #[wasm_bindgen_test]
-async fn context_async_message_handler() {
+async fn context_message_handler_async() {
 	assert!(Message::has_array_buffer_support().is_ok());
 
 	let request = Flag::new();
@@ -378,6 +513,78 @@ async fn handle_multi_message() {
 	response.await;
 
 	worker.terminate();
+}
+
+/// Multiple messages in
+/// [`WorkerHandleRef::transfer_messages()`](wasm_worker::WorkerHandleRef::transfer_messages).
+#[wasm_bindgen_test]
+async fn handle_ref_multi_message() -> Result<()> {
+	assert!(Message::has_array_buffer_support().is_ok());
+
+	let request = Flag::new();
+	let response = Flag::new();
+
+	let worker = WorkerBuilder::new()?
+		.message_handler_async({
+			let request = request.clone();
+			move |worker, _| {
+				let request = request.clone();
+				let worker = worker.clone();
+
+				async move {
+					request.await;
+
+					let buffer_1 = ArrayBuffer::new(1);
+					let array = Uint8Array::new(&buffer_1);
+					array.copy_from(&[1]);
+
+					let buffer_2 = ArrayBuffer::new(2);
+					let array = Uint8Array::new(&buffer_2);
+					array.copy_from(&[2; 2]);
+
+					let buffer_3 = ArrayBuffer::new(3);
+					let array = Uint8Array::new(&buffer_3);
+					array.copy_from(&[3; 3]);
+
+					worker
+						.transfer_messages([buffer_1, buffer_2, buffer_3])
+						.unwrap();
+				}
+			}
+		})
+		.spawn_async({
+			let request = request.clone();
+			let response = response.clone();
+
+			|context| async move {
+				context.transfer_messages([ArrayBuffer::new(1)]).unwrap();
+
+				context.set_message_handler(move |_, event| {
+					let messages = event.messages().unwrap().into_iter();
+					assert_eq!(messages.len(), 3);
+
+					for (index, message) in (1_u8..).zip(messages) {
+						let buffer: ArrayBuffer = message.serialize_as().unwrap();
+
+						let array = Uint8Array::new(&buffer);
+						assert_eq!(buffer.byte_length(), index.into());
+						let mut output = [0; 3];
+						array.copy_to(&mut output[..index.into()]);
+						assert!(output[..index.into()].iter().all(|value| *value == index));
+					}
+
+					response.signal();
+				});
+
+				request.signal();
+			}
+		});
+
+	response.await;
+
+	worker.terminate();
+
+	Ok(())
 }
 
 /// Multiple messages in [`WorkerContext::transfer_messages()`].
