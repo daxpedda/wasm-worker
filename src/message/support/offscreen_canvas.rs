8cceb5af1f81cc64d2dab804f06405905cef855e
dAxpeDDa <daxpedda@gmail.com>
2023-02-23 18:34:06 +0100

    Unnecessary exports for `OffscreenCanvas` support

--- src/message/support/offscreen_canvas.rs
+++ src/message/support/offscreen_canvas.rs
@@ -1,6 +1,4 @@
 use once_cell::sync::Lazy;
-use wasm_bindgen::prelude::wasm_bindgen;
-use wasm_bindgen::JsValue;
 use web_sys::OffscreenCanvas;
 
 use super::super::SupportError;
@@ -8,15 +6,6 @@ use crate::global::Global;
 
 pub(in super::super) fn support() -> Result<(), SupportError> {
 	static SUPPORT: Lazy<Result<(), SupportError>> = Lazy::new(|| {
-		#[wasm_bindgen]
-		extern "C" {
-			#[allow(non_camel_case_types)]
-			type __wasm_worker_OffscreenCanvasGlobal;
-
-			#[wasm_bindgen(method, getter, js_name = OffscreenCanvas)]
-			fn offscreen_canvas(this: &__wasm_worker_OffscreenCanvasGlobal) -> JsValue;
-		}
-
 		if Global::new().offscreen_canvas().is_undefined() {
 			return Err(SupportError::Unsupported);
 		}
