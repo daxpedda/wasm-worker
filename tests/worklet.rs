4f93b83c849f0d701dd744eb2a99acff12a58652
dAxpeDDa <daxpedda@gmail.com>
2023-03-04 16:26:48 +0100

    Add more worklet tests

--- tests/worklet.rs
+++ tests/worklet.rs
@@ -14,7 +14,7 @@ use crate::util::SIGNAL_DURATION;
 
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
 
-/// [`WorkletExt::add_wasm`].
+/// [`WorkletExt::add_wasm()`].
 #[wasm_bindgen_test]
 async fn basic() {
 	let flag = Flag::new();
@@ -34,7 +34,7 @@ async fn basic() {
 	flag.await;
 }
 
-/// [`WorkletExt::add_wasm`] returning [`WorkletInitError`].
+/// [`WorkletExt::add_wasm()`] returning [`WorkletInitError`].
 #[wasm_bindgen_test]
 async fn failure() {
 	let context =
@@ -96,8 +96,28 @@ fn context_fail() {
 	assert!(WorkletContext::new().is_none());
 }
 
-/// [`WorkletUrl::new()`], [`WorkletUrl::new()`] and
-/// [`WorkletExt::add_wasm_with_url()`].
+/// [`WorkletBuilder::add()`].
+#[wasm_bindgen_test]
+async fn builder() {
+	let flag = Flag::new();
+
+	let context =
+		OfflineAudioContext::new_with_number_of_channels_and_length_and_sample_rate(1, 1, 8000.)
+			.unwrap();
+	WorkletBuilder::new()
+		.add(&context, {
+			let flag = flag.clone();
+			move |_| flag.signal()
+		})
+		.unwrap()
+		.await
+		.unwrap();
+
+	flag.await;
+}
+
+/// [`WorkletUrl::new()`], [`WorkletBuilder::new_with_url()`] and
+/// [`WorkletBuilder::add()`].
 #[wasm_bindgen_test]
 async fn builder_url() {
 	// We will just use the default `WorkletUrl` but build it ourselves.
