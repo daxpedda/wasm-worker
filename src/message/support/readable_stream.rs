7da7acea64543328ca79a0b8a25b77cd5cfb3f97
daxpedda <daxpedda@gmail.com>
2024-01-12 23:28:42 +0100

    Partly remove unnecessary `web_sys_unstable_apis`

--- src/message/support/readable_stream.rs
+++ src/message/support/readable_stream.rs
@@ -1,23 +1,10 @@
 use once_cell::sync::OnceCell;
-#[cfg(web_sys_unstable_apis)]
 use web_sys::ReadableStream;
-#[cfg(not(web_sys_unstable_apis))]
-use {wasm_bindgen::prelude::wasm_bindgen, wasm_bindgen::JsValue};
 
 use super::super::MessageSupportError;
 use crate::global::{Global, GlobalContext};
 
 pub(in super::super) fn support() -> Result<bool, MessageSupportError> {
-	#[cfg(not(web_sys_unstable_apis))]
-	#[wasm_bindgen]
-	extern "C" {
-		#[wasm_bindgen(js_name = ReadableStream)]
-		type ReadableStreamExt;
-
-		#[wasm_bindgen(catch, constructor, js_class = "ReadableStream")]
-		fn new() -> Result<ReadableStreamExt, JsValue>;
-	}
-
 	static SUPPORT: OnceCell<bool> = OnceCell::new();
 
 	SUPPORT
@@ -34,10 +21,7 @@ pub(in super::super) fn support() -> Result<bool, MessageSupportError> {
 				GlobalContext::Worklet => Err(MessageSupportError::Context),
 			})?;
 
-			#[cfg(web_sys_unstable_apis)]
 			let stream = ReadableStream::new().unwrap();
-			#[cfg(not(web_sys_unstable_apis))]
-			let stream = ReadableStreamExt::new().unwrap();
 
 			Ok(super::test_support(&stream))
 		})
--- src/message/support/transform_stream.rs
+++ src/message/support/transform_stream.rs
@@ -1,23 +1,10 @@
 use once_cell::sync::OnceCell;
-#[cfg(web_sys_unstable_apis)]
 use web_sys::TransformStream;
-#[cfg(not(web_sys_unstable_apis))]
-use {wasm_bindgen::prelude::wasm_bindgen, wasm_bindgen::JsValue};
 
 use super::super::MessageSupportError;
 use crate::global::{Global, GlobalContext};
 
 pub(in super::super) fn support() -> Result<bool, MessageSupportError> {
-	#[cfg(not(web_sys_unstable_apis))]
-	#[wasm_bindgen]
-	extern "C" {
-		#[wasm_bindgen(js_name = TransformStream)]
-		type TransformStreamExt;
-
-		#[wasm_bindgen(catch, constructor, js_class = "TransformStream")]
-		fn new() -> Result<TransformStreamExt, JsValue>;
-	}
-
 	static SUPPORT: OnceCell<bool> = OnceCell::new();
 
 	SUPPORT
@@ -34,10 +21,7 @@ pub(in super::super) fn support() -> Result<bool, MessageSupportError> {
 				GlobalContext::Worklet => Err(MessageSupportError::Context),
 			})?;
 
-			#[cfg(web_sys_unstable_apis)]
 			let stream = TransformStream::new().unwrap();
-			#[cfg(not(web_sys_unstable_apis))]
-			let stream = TransformStreamExt::new().unwrap();
 
 			Ok(super::test_support(&stream))
 		})
